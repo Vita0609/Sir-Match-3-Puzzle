ac2c404b724289d6cf96aa04c9abb181eb77b984
Vita <nikaangel295@gmail.com>
2024-12-23 12:31:32 +0000

    add

--- src/js/header.js
+++ src/js/header.js
@@ -1,32 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const burgerToggle = document.querySelector('#burger-toggle'); // Чекбокс для управления меню
+  const burgerToggle = document.querySelector('#burger-toggle');
   const navToggle = document.querySelector('.nav-toggle');
-  const navList = document.querySelector('.nav-list'); // Кнопка для бургер-меню
+  const navList = document.querySelector('.nav-list');
 
-  // Когда кликаем на кнопку бургер-меню, переключаем состояние чекбокса
   navToggle.addEventListener('click', () => {
     burgerToggle.checked = !burgerToggle.checked;
-    burgerToggle.checked ? navList.classList.add('nav-open') : navList.classList.remove('nav-open');// Переключаем состояние чекбокса
+
   });
 
-  // Плавный переход между секциями
   const navLinks = document.querySelectorAll('.nav-link');
   navLinks.forEach(link => {
     link.addEventListener('click', function (e) {
-      e.preventDefault(); // Отменяем стандартный переход
+      e.preventDefault();
 
-      // Получаем ID секции и прокручиваем к ней
-      const targetId = this.getAttribute('href').substring(1); // Убираем символ #
+      const targetId = this.getAttribute('href').substring(1);
       const targetElement = document.getElementById(targetId);
 
       if (targetElement) {
-        // Прокручиваем к секции
         targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
-
-      // Закрываем бургер-меню после клика на мобильных устройствах
+}
       if (burgerToggle.checked) {
-        burgerToggle.checked = false; // Скрываем меню
+        burgerToggle.checked = false;
       }
     });
   });
