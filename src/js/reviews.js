82ab0cfb0fd0a3656115bfa4bdf329c3df278fb2
Vita <nikaangel295@gmail.com>
2024-12-27 09:48:23 +0000

    add

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -8,6 +8,10 @@ new Swiper('.swiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   spaceBetween: 20,
   breakpoints: {
     320: {
